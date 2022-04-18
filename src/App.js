3459b6c8bb95a96a249999812caa7a61ca9ce202
eelojs <j__is_00@naver.com>
2022-04-18 21:27:33 +0900

    04 깃허브에 리액트 앱 업로드하기

--- src/App.js
+++ src/App.js
@@ -1,36 +1,74 @@
 import React from "react";
+import propTypes from 'prop-types'
 
 
 // function Fruit(props){
 //   console.log(props)
 //   return (
 //     <h1>{props.fav} 싱싱해용</h1>
-  
 
-  
+
+
 //   )
-  
+
 // }
-function Orange(){
-  return <h1>오렌지먹고 있어요 냉장고앞에서,,</h1>
+function Fruit({ name, picture, rating}) {
+    return (
+        <div>
+            <h2>{name}맛있어요</h2>
+            <h4>{rating} /5.0 </h4>
+            <img src={picture} alt={name} />
+        </div>
+    )
 }
-function Fruit({fav}){
-  return (<h1>{fav}싱싱해용</h1>)
-}
+
+const fruitILike = [
+    {
+        id: 1,
+        name: "바나나",
+        image:"http://qwerew.cafe24.com/images/banana.png",
+        rating: 2.5,
+    },
+    {   
+        id:2,
+        name:'오렌지',
+        image:'http://qwerew.cafe24.com/images/orange.png',
+        rating: 4,
+
+    },
+    {
+        id:3,
+        name:'사과',
+        image:'http://qwerew.cafe24.com/images/apple.png',
+        rating: 3.5,
+
+    },
+    {
+        id:4,
+        name:'메론',
+        image:'http://qwerew.cafe24.com/images/melon.jpg',
+        rating: 5,
+
+    },
+
+]
+
+{/* function renderFruit(dish){
+return <Fruit key={dish.id} name={dish.name} picture={dish.image}/>} */}
+
 function App() {
-  return (
-    <div>
-      <h1>짱구야 짱아 어딨니</h1>
-      <Fruit fav='바나나' />
-      <Fruit fav='망고' />
-      <Fruit fav='수박' />
-      <Fruit fav='체리' />
-      <Fruit fav='구아바' />
-      <Fruit fav='복숭아' />
-
-
-    </div>
-  );
+    return (
+        <div>
+            {fruitILike.map(dish=> (
+            <Fruit key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
+            ))}
+        </div>
+    )
+}
+Fruit.propTypes={
+    name: propTypes.string.isRequired,
+    picture: propTypes.string.isRequired,
+    rating:  propTypes.number.isRequired,
 }
 
-export default App;
+export default App
