6b9d34addfcb96894f8a7d24beda1a127afc1bd8
eelojs <j__is_00@naver.com>
2022-04-24 23:42:13 +0900

    06-2 깃허브에 리액트 앱 업로드하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
+import "./App.css"
 
 class App extends React.Component{
     state ={
@@ -7,7 +9,7 @@ class App extends React.Component{
         movies:[],
     }
     getMovies=async()=>{
-        const {data:{data:{movies}}}= await axios.get('https://yts-proxy.now.sh/list_movies.json');
+        const {data:{data:{movies}}}= await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
         this.setState({movies:movies, isLoading: false});
     }
 
@@ -19,15 +21,30 @@ class App extends React.Component{
     render(){
         const{ isLoading }= this.state;
 
-        return(
-            <div>
-                {isLoading? '로딩중...' : '로딩완료'}
-            </div>
-        )
-        
-    }
-}
-
+        return (
+            <section className="container">
+                {isLoading ? (
+                <div className="loader">
+                    <span className="loader_text">"LOADING..."</span>
+                </div>
+                ) : (
+                <div className="movies">
+                    {movies.map((movie) => (
+                    <Movie
+                        key={movie.id}
+                        id={movie.id}
+                        year={movie.year}
+                        title={movie.title}
+                        summary={movie.summary}
+                        poster={movie.medium_cover_image}
+                    />
+                    ))}
+                </div>
+                )}
+            </section>
+            );
+        }
+            }
 
 
 export default App
--- src/Movie.js
+++ src/Movie.js
@@ -1,15 +1,23 @@
 import react from "react";
 import PropTypes from 'prop-types';
+import "./Movie.css"
 
-function Movie(){
-    return<h1></h1>
+function Movie({title,year,summary,poster}){
+    return(
+        <div className="movie_data">
+        <img src={poster} alt={title} title={title}></img>
+        <h3 className="movie_title">{title}</h3>
+        <h5 className="movie_year">{year}</h5>
+        <p className="movie_summary">{summary}</p>
+        </div>
+    );
+
 }
-// title,summery,medium_cover_image->poster
+// title,summary,medium_cover_image->poster
 Movie.PropTypes={
-    id:PropTypes.number.isRequired,
     year:PropTypes.number.isRequired,
     title:PropTypes.string.isRequired,
-    summery:PropTypes.string.isRequired,
+    summary:PropTypes.string.isRequired,
     poster:PropTypes.string.isRequired,
 };
 
