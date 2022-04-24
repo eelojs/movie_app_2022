fdf4773e3daf64d9dbe38eeda04556fcf24cd123
eelojs <j__is_00@naver.com>
2022-04-25 01:46:04 +0900

    리액트영화앱 최종완성

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import './App.css';
-import { HashRouter, Route } from 'react-router-dom';
-import About from './routes/About';
-import Home from './routes/Home';
-import Navigation from './components/Navigation';
-import Detail from './routes/Detail';
+import React from "react"
+import axios from "axios"
+import { HashRouter, Route } from "react-router-dom";
+
+import About from "./routes/About"
+import Home from "./routes/Home"
+import Detail from "./routes/Detail"
+import Navigation from "./components/Navigation";
+
+import "./App.css"
+
 
 function App() {
-  return (
-    <HashRouter>
-      <Navigation />
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/movie-detail" component={Detail} />
-    </HashRouter>
-  );
+	return(
+		<HashRouter>
+			<Navigation />
+			<Route path="/" exact={true} component={Home} />
+			<Route path="/about" component={About} />
+			<Route path="/movie-detail" component={Detail} />
+		</HashRouter>
+	)
 }
-
-export default App;
